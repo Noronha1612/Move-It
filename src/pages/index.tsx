1706d809e3116d298db75d0e0015d0bd63bf8871
NoronhaGCP <inc.691@gmail.com>
2021-02-23 14:09:46 -0300

    create CompletedChallenges component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { LeftContainer, MainContainer } from "../AppStyles";
+import CompletedChallenges from "../components/CompletedChallenges";
 
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
@@ -11,6 +12,7 @@ export default function Home() {
       <section>
         <LeftContainer>
           <Profile />
+          <CompletedChallenges />
         </LeftContainer>
         <div>
 
